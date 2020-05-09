e581e6cdb2ae82154c5dce78b2a2264923ef5acd
Frank Behrens <fbehrens@gmail.com>
2020-05-09 12:12:00 +0200

    new Command Time

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('activate myCodeExt1');
-
-	let disposable = vscode.commands.registerCommand('mycodeext1.helloWarning', () => {
+	console.log('now');
+	context.subscriptions.push(vscode.commands.registerCommand('mycodeext1.helloWarning', () => {
 		vscode.window.showWarningMessage('warning from myCodeExt1');
-	});
-	context.subscriptions.push(disposable);
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('mycodeext1.Time', () => {
+        let now = new Date().toLocaleString();
+		vscode.window.showInformationMessage(`the time is: ${now}`);
+	}));
 }
 
 export function deactivate() {}
