f7a437e56077c79d8a0546048206e884f16c0b35
Frank Behrens <fbehrens@gmail.com>
2022-01-20 22:53:27 +0100

    windows terminal has own escape sequence

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,18 @@ import u from './util';
 var util = require('util');
 
 export function activate(context: vscode.ExtensionContext) {
-
-    console.log('activate2');
 	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAndRepeat', () => {
+        // https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#cursor-keys
+        const escape = vscode.window.activeTextEditor?.document.languageId === "powershell" ? "O" : "[";
+        const seq = `\u001b${escape}A\n`;
         vscode.commands.executeCommand('workbench.action.files.save').then( () =>
-        vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text":"\u001b[A\u000D" }));
+        vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text": seq }));
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAllAndRepeat', () => {
+        const escape = vscode.window.activeTextEditor?.document.languageId === "powershell" ? "O" : "[";
+        const seq = `\u001b${escape}A\n`;
         vscode.commands.executeCommand('workbench.action.files.saveAll').then( () =>
-        vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text":"\u001b[A\u000D" }));
+        vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text":seq }));
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand('agili.play', () => {
         // vscode.env.clipboard.readText()
