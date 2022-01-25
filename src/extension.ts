531c5038a00dbd7ca913f960662ff35ba29da70e
Frank Behrens <fbehrens@gmail.com>
2022-01-25 16:26:00 +0100

    commands with language parameter

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,23 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAndRepeat', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAndRepeat', (language : string = '') => {
         // https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#cursor-keys
-        const escape = vscode.window.activeTextEditor?.document.languageId === "powershell" ? "O" : "[";
+        const escape = language === "powershell" ? "O" : "[";
         const seq = `\u001b${escape}A\n`;
         vscode.commands.executeCommand('workbench.action.files.save').then( () =>
         vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text": seq }));
 	}));
-	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAllAndRepeat', () => {
-        const escape = vscode.window.activeTextEditor?.document.languageId === "powershell" ? "O" : "[";
+	context.subscriptions.push(vscode.commands.registerCommand('agili.saveAllAndRepeat', (language : string = '') => {
+        const escape = language === "powershell" ? "O" : "[";
         const seq = `\u001b${escape}A\n`;
         vscode.commands.executeCommand('workbench.action.files.saveAll').then( () =>
         vscode.commands.executeCommand('workbench.action.terminal.sendSequence',{"text":seq }));
 	}));
+    context.subscriptions.push(vscode.commands.registerCommand('agili.hello', (ispowershell: boolean = false) => {
+        const escape = ispowershell ? 'O' : '[';
+        console.log(`Hello1 ${escape}!!!`);
+    }));
 }
 
 export function deactivate() {}
